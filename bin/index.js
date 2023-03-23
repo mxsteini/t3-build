ff860002ad299c1d82608b52379ffc39d4eb04cb
Michael Stein <info@michaelstein-itb.de>
2023-03-23 20:33:55 +0100

    first version

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S node --trace-warnings
+
+'use strict';
+
+import Main from '../index.js'
+
+new Main()
---
+++ index.js
@@ -0,0 +1,33 @@
+import Config from './lib/config.js'
+import Check from './lib/check.js'
+import dotenv from 'dotenv'
+import Build from './lib/build.js'
+import Watch from './lib/watch.js'
+export default class Main {
+    config = {}
+
+    constructor() {
+        dotenv.config()
+        this.config = new Config()
+
+        const check = new Check()
+
+        if (check.checkDirectories(this.config)) {
+            switch (this.config.mode) {
+                case 'build':
+                    new Build(this.config)
+                    break
+                case 'watch':
+                    new Build(this.config)
+                    new Watch(this.config)
+                    break
+            }
+        }
+        return true
+    }
+}
+
+
+
+
+
